ab67f5e9726393a2e11dd3f0f63d2f498ae6864d
Kamil Borowiec <kborowiec95@gmail.com>
2017-05-20 00:08:47 +0200

    Preparing special class for GoogleResultsGetter

---
+++ GoogleResultsGetter.py
@@ -0,0 +1,25 @@
+import urllib.request
+import urllib.error
+
+
+class GoogleResultsGetter:
+    def __init__(self, google_api_user_key, custor_search_id):
+        self.key = google_api_user_key
+        self.custom_search_id = custor_search_id
+
+    def get_from_api(self, request):
+        google_request = request.replace(" ", "+")
+        url = "https://www.googleapis.com/customsearch/v1?key=" + self.key + \
+              "&cx=" + self.custom_search_id + \
+              "&q=" + google_request
+        return urllib.request.urlopen(url).read().decode("utf-8")
+
+    def number_of_google_searches(self, sentence):
+        for json_line in self.get_from_api(sentence).split("\n"):
+            if "totalResults" in json_line:
+                return int(json_line.split("\"")[3])
+        raise ValueError("Wrong json format")
+
+
+def google_searcher():
+    return GoogleResultsGetter("AIzaSyCZ9HvClGcCYqVxT9SruazjoHb9xNewuOM", "008201436329454125027:r1b12zwr7ko")
--- main.py
+++ main.py
@@ -1,38 +1,11 @@
-import urllib.request
-import urllib.error
+from GoogleResultsGetter import *
 
-api_key = "AIzaSyCZ9HvClGcCYqVxT9SruazjoHb9xNewuOM"
-file_name = "sample_result"
-
-
-def get_from_api(key, request):
-    google_request = request.replace(" ", "+")
-    url = "https://www.googleapis.com/customsearch/v1?key=" + key \
-          + "&cx=008201436329454125027:r1b12zwr7ko&q=" + google_request
-    return urllib.request.urlopen(url).read().decode("utf-8")
-    #return url
-
-def lines(json_text):
-    return json_text.split("\n")
-
-
-def get_from_file(name):
-    with open(name, 'r') as json_text:
-        res = json_text.read()
-    return res
-
-
-def get_number_of_google_searches(word):
-    for line in lines(get_from_api(api_key, word)):
-        if "totalResults" in line:
-            return line.split("\"")[3]
-    raise ValueError("Wrong json format")
+gs = google_searcher()
 
 try:
-    true = int(get_number_of_google_searches("Pope is dead"))
-    false = int(get_number_of_google_searches("Pope is not dead"))
-    true_coefficient = true / (true + false) * 100
-    print("This message is true for : " + str(true) + " vs " + str(false) + " true vs false")
+    true_score = gs.number_of_google_searches("Cracow is in Poland")
+    false_score = gs.number_of_google_searches("Cracow is not in Poland")
+    print("This message is true for : " + str(true_score) + " vs " + str(false_score) + " true vs false")
 except urllib.error.URLError:
     print("Connection problems")
 except ValueError:
